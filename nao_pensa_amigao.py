8b28bd73bdfcccb7e6c4826a0172dbb6cb71f275
Eduardo Bonachela <dudubonachela@gmail.com>
2022-09-27 22:46:08 -0300

    stream audio instead of download entire audio file

--- nao_pensa_amigao.py
+++ nao_pensa_amigao.py
@@ -3,9 +3,20 @@ import discord
 import os
 import math
 import sys
+import youtube_dl
 from discord.ext import commands
 from modules.YTDL import YTDL
 from modules.BotConfig import BotConfig
+
+from youtube_dl import YoutubeDL
+from requests import get
+
+def search(arg):
+    with YoutubeDL({'format': 'bestaudio', 'noplaylist':'True'}) as ydl:
+        try: get(arg)
+        except: info = ydl.extract_info(f"ytsearch:{arg}", download=False)['entries'][0]
+        else: info = ydl.extract_info(arg, download=False)
+    return (info, info['formats'][0]['url'])
 
 def main():
     intents = discord.Intents().all()
@@ -20,20 +31,34 @@ def main():
     @bot.event
     async def on_ready():
         print(f'{bot.user} has connected to Discord!')
-      
+
     @bot.command(name='play', help='Solta o som dj')
-    async def play(ctx, url):
+    async def play(ctx,*,arg):
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+        YDL_OPTIONS = {'format': 'bestaudio/best', 'noplaylist':'True'}
+
+        url1 = arg.split(" ")
+        url2 = url1[0].replace("[","")
+        url = url2.replace("]","")
+        
         channel = ctx.message.author.voice.channel
-        voice = await channel.connect()
+        voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else:
+            voice = await channel.connect()
+        ydl = youtube_dl.YoutubeDL(YDL_OPTIONS)
+        with ydl:
+            info = ydl.extract_info(url, download=False)
+            I_URL = info['formats'][0]['url']
+            print(I_URL)
+            source = await discord.FFmpegOpusAudio.from_probe(I_URL, **FFMPEG_OPTIONS)
+            voice.play(source)
+            while voice.is_playing():
+                await asyncio.sleep(1)
 
-        async with ctx.typing():
-            filename = await YTDL.from_url(url, loop=client.loop)
-            voice.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
-
-        while voice.is_playing():
-            await asyncio.sleep(1)
-
-        await voice.disconnect()
+            await voice.disconnect()
     
     @bot.command(name='add', help='Adiciona comando para tocar áudio do youtube')
     async def add(ctx, name, url):
@@ -42,11 +67,8 @@ def main():
             return
 
         serverID = str(ctx.message.guild.id)
-
-        async with ctx.typing():
-            filename = await YTDL.from_url(url, loop=client.loop)
         
-        if audioCommands.addCommand(serverID, name, filename):
+        if audioCommands.addCommand(serverID, name, url):
             await ctx.send(f"{name} adicionado com sucesso!")
             return
         
@@ -73,7 +95,7 @@ def main():
             return
 
         for key in audioCommands.m_config[serverID]:
-            members.append(f"- {key} <https://youtu.be/{audioCommands.m_config[serverID][key][6:17]}>")
+            members.append(f"- {key} {audioCommands.m_config[serverID][key]}")
 
         if len(members) == 0:
             await ctx.send('Nenhum comando adicionado até o momento. Utilize !add para adicionar comandos novos.')
@@ -131,14 +153,28 @@ def main():
 
         if message.content[0] == '!' and message.content[1:] in audioCommands.m_config[serverID]:
             channel = message.author.voice.channel
-            voice = await channel.connect()
+            url = audioCommands.m_config[serverID][message.content[1:]]
 
-            voice.play(discord.FFmpegPCMAudio(executable="ffmpeg", source=audioCommands.m_config[serverID][message.content[1:]]))
+            FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
-            while voice.is_playing():
-                await asyncio.sleep(1)
+            YDL_OPTIONS = {'format': 'bestaudio/best', 'noplaylist':'True'}
+            
+            voice = discord.utils.get(client.voice_clients, guild=message.guild)
+            if voice and voice.is_connected():
+                await voice.move_to(channel)
+            else:
+                voice = await channel.connect()
+            ydl = youtube_dl.YoutubeDL(YDL_OPTIONS)
+            with ydl:
+                info = ydl.extract_info(url, download=False)
+                I_URL = info['formats'][0]['url']
+                print(I_URL)
+                source = await discord.FFmpegOpusAudio.from_probe(I_URL, **FFMPEG_OPTIONS)
+                voice.play(source)
+                while voice.is_playing():
+                    await asyncio.sleep(1)
 
-            await voice.disconnect()
+                await voice.disconnect()
 
             return
 
